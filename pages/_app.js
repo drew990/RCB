90630af04541d31f4382d96cc6244ea71ec32fc2
Andrew Banagas <andrewbanagas40@gmail.com>
2023-05-15 23:44:52 -0700

    Updated Layout Error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Layout from "../components/layout/layout";
+import Layout from "../components/Layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
